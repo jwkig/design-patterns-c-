62f7803ba5cf0d9e2428a05946a425c36d17aa68
Ilya Kovalenko <da_ikovalenko@phreesia.com>
2026-06-05 23:01:48 +0300

    Added 3Units

---
+++ 03 Factories/Factory/Factory/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Factory;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Person(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+public class PersonFactory
+{
+    private int _index = 0;
+    
+    public Person CreatePerson(string name)
+    {
+        return new Person(_index++, name);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 04 Prototype/Prototype/Prototype/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Prototype;
+
+public static class IDeepCopyableExtension 
+{
+    public static T DeepCopy<T>(this IDeepCopyable<T> self) where T : new()
+    {
+        return self.DeepCopy();
+    }
+}
+public interface IDeepCopyable<T> where T : new()
+{
+    void CopyTo(T target);
+
+    T DeepCopy()
+    {
+        T copy = new T();
+        CopyTo(copy);
+        return copy;
+    }
+}
+
+public class Point : IDeepCopyable<Point>
+{
+    public int X, Y;
+
+    public Point()
+    {
+    }
+
+    public void CopyTo(Point target)
+    {
+        target.X = X;
+        target.Y = Y;
+    }
+}
+
+public class Line : IDeepCopyable<Line>
+{
+    public Point Start, End;
+
+    public Line()
+    {
+    }
+
+    public void CopyTo(Line target)
+    {
+        target.Start = Start.DeepCopy();
+        target.End = End.DeepCopy();
+    }
+
+    public Line DeepCopy()
+    {
+        return ((IDeepCopyable<Line>)this).DeepCopy();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
